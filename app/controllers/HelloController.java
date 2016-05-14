c33277cde39fc6c6a90e0cdf18169c09a12fdfd3
ZHIXUAN YU <yu343@purdue.edu>
2016-05-14 18:07:27 +0800

    hello world

---
+++ app/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import play.mvc.*;
+
+import views.html.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the page that prints hello world
+ */
+public class HelloController extends Controller {
+	private final String hello = "Hello World!";
+    
+    public Result index() {
+        return ok(hello);
+    }
+
+}
